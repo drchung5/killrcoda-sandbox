fde60466df79849b7698a679e3db6e288e46cc1d
David Chung <dchung@dchung.com>
2024-06-05 17:51:10 -0500

    modified install

--- AC100-lab01/foreground.sh
+++ AC100-lab01/foreground.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-exec > /dev/null 2>&1
-
 echo "foreground.sh"
 
 echo -n "Install JDK 11 "
 
 apt-get update 
 
-sudo apt-get install -y openjdk-11-jdk-headless
+sudo apt-get install -y openjdk-11-jdk openjdk-11-jdk < "/dev/null" > /dev/null 2>&1
 
-# until [[ -e /usr/lib/jvm/java-11-openjdk-amd64 && -e /usr/bin/java ]]
-# do
-#      sleep 1
-#      echo -n '.'
-# done
+until [[ -e /usr/lib/ && -e /usr/bin/java ]]
+do
+     sleep 1
+     echo -n '.'
+done
 
-exec > /dev/tty 2>&1
 
 echo "JDK 11 installation complete"
