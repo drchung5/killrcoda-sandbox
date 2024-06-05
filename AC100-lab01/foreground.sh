4f1c2c1fe312a2746f775ec340888ac9cb662f95
David Chung <dchung@dchung.com>
2024-06-05 18:05:02 -0500

    added jre

--- AC100-lab01/foreground.sh
+++ AC100-lab01/foreground.sh
@@ -6,7 +6,7 @@ echo -n "Install JDK 11 "
 
 apt-get update > /dev/null 2>&1
 
-sudo apt-get install -y openjdk-11-jdk openjdk-11-jdk < "/dev/null" > /dev/null 2>&1
+sudo apt-get install -y openjdk-11-jdk openjdk-11-jre < "/dev/null" > /dev/null 2>&1
 
 until [[ -e /usr/lib/jvm/openjdk-11 && -e /usr/bin/java ]]
 do
