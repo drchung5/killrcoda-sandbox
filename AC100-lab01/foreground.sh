3b06036ff49b81b0ec8b10666fefb351a11a775e
David Chung <dchung@dchung.com>
2024-06-05 17:41:36 -0500

    still trying to supress output

--- AC100-lab01/foreground.sh
+++ AC100-lab01/foreground.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-set +x
 
 echo "foreground.sh"
 
 echo -n "Install JDK 11 "
-apt-get update > /dev/null 2>&1
-sudo apt-get install -y openjdk-11-jdk-headless  < "/dev/null" > /dev/null 2>&1 
 
-until [[ -e /usr/lib/jvm/java-11-openjdk-amd64 && -e /usr/bin/java ]]
-do
-     sleep 1
-     echo -n '.'
-done
+exec > /dev/null 2>&1
+
+apt-get update 
+sudo apt-get install -y openjdk-11-jdk-headless
+
+# until [[ -e /usr/lib/jvm/java-11-openjdk-amd64 && -e /usr/bin/java ]]
+# do
+#      sleep 1
+#      echo -n '.'
+# done
+
+exec > /dev/tty 2>&1
 
 echo "JDK 11 installation complete"
-
-set -x
