81af8bea283b1ae7ecd448954a9f83102198bc55
David Chung <dchung@dchung.com>
2024-06-05 17:48:15 -0500

    echo

--- AC100-lab01/foreground.sh
+++ AC100-lab01/foreground.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+exec > /dev/null 2>&1
 
 echo "foreground.sh"
 
 echo -n "Install JDK 11 "
 
-exec > /dev/null 2>&1
-
 apt-get update 
+
 sudo apt-get install -y openjdk-11-jdk-headless
 
 # until [[ -e /usr/lib/jvm/java-11-openjdk-amd64 && -e /usr/bin/java ]]
