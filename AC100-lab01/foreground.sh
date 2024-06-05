617896e23ae5c687c2ff0d09e71e24447ce51ec9
David Chung <dchung@dchung.com>
2024-06-05 17:36:38 -0500

    added command supression

--- AC100-lab01/foreground.sh
+++ AC100-lab01/foreground.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+set +x
+
 echo "foreground.sh"
 
 echo -n "Install JDK 11 "
 apt-get update > /dev/null 2>&1
 sudo apt-get install -y openjdk-11-jdk-headless  < "/dev/null" > /dev/null 2>&1 
 
-until [[ -e java-11-openjdk-amd64 && -e /usr/bin/java ]]
+until [[ -e /usr/lib/jvm/java-11-openjdk-amd64 && -e /usr/bin/java ]]
 do
      sleep 1
      echo -n '.'
 done
 
 echo "JDK 11 installation complete"
+
+set -x
