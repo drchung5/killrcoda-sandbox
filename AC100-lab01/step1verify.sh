f9cd6767f774075d014a80abbd7843bed5f4d20f
David Chung <dchung@dchung.com>
2024-06-06 14:12:57 -0500

    echo

--- AC100-lab01/step1verify.sh
+++ AC100-lab01/step1verify.sh
@@ -3,8 +3,8 @@
 TARGET_DIR="/home/cassandra-userbbbb"
 CURRENT_DIR=$(pwd)
 
-echo TARGET_DIR
-echo CURRENT_DIR
+echo TARGET_DIR /dev/tty 2>&1
+echo CURRENT_DIR /dev/tty 2>&1
 
 if[ "$CURRENT_DIR" == "$TARGET_DIR" ] then
   exit 0
