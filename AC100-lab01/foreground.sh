65b2d185ba476fa5006a3db1a0e1d36a44274c56
David Chung <dchung@dchung.com>
2024-06-05 21:48:53 -0500

    newlines

--- AC100-lab01/foreground.sh
+++ AC100-lab01/foreground.sh
@@ -9,6 +9,6 @@ do
      echo -n '.' > /dev/tty 2>&1
 done
 
-echo -ne "\n\tLab environment ready!" > /dev/tty 2>&1
+echo -ne "\n\n\tLab environment ready!\n\n" > /dev/tty 2>&1
 
 exec > /dev/tty 2>&1
