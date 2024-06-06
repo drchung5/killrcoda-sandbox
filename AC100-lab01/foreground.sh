a16b6f13f5dd72cfeae81ae00976a4fd90902344
David Chung <dchung@dchung.com>
2024-06-05 21:40:11 -0500

    escape characters

--- AC100-lab01/foreground.sh
+++ AC100-lab01/foreground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 exec > /dev/null 2>&1
 
-echo -n "Configuring lab envronment " > /dev/tty 2>&1
+echo -ne "\n\tConfiguring lab envronment " > /dev/tty 2>&1
 
 until [[ -e /usr/lib/jvm && -e /usr/bin/java ]]
 do
@@ -9,7 +9,6 @@ do
      echo -n '.' > /dev/tty 2>&1
 done
 
-echo ""  > /dev/tty 2>&1
-echo "Lab environment ready!"  > /dev/tty 2>&1
+echo -e "\n\tLab environment ready!\n"  > /dev/tty 2>&1
 
 exec > /dev/tty 2>&1
