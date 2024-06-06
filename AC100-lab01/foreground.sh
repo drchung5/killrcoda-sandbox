5ccc6e688eb977331e41c42d23693a636fc1ffcf
David Chung <dchung@dchung.com>
2024-06-05 21:22:43 -0500

    commented out foreground

--- AC100-lab01/foreground.sh
+++ AC100-lab01/foreground.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-exec > /dev/null 2>&1
+# exec > /dev/null 2>&1
 
-clear
+# clear
 
-echo "" > /dev/tty 2>&1
-echo "Update package lists"> /dev/tty 2>&1
-apt-get update 
+# echo "" > /dev/tty 2>&1
+# echo "Update package lists"> /dev/tty 2>&1
+# apt-get update 
 
-echo "" > /dev/tty 2>&1
-echo -n "Install JDK 11 " > /dev/tty 2>&1
+# echo "" > /dev/tty 2>&1
+# echo -n "Install JDK 11 " > /dev/tty 2>&1
 
+# # Loop to check for the existence of JDK installation directories
+# until [[ -e /usr/lib/jvm && -e /usr/bin/java ]]
+# do
+#      sleep 1
+#      echo -n '.' > /dev/tty 2>&1
+# done
 
+# echo "" > /dev/tty 2>&1
+# echo "JDK 11 installation complete" > /dev/tty 2>&1
+# echo "" > /dev/tty 2>&1
 
-# Loop to check for the existence of JDK installation directories
-until [[ -e /usr/lib/jvm && -e /usr/bin/java ]]
-do
-     sleep 1
-     echo -n '.' > /dev/tty 2>&1
-done
-
-echo "" > /dev/tty 2>&1
-echo "JDK 11 installation complete" > /dev/tty 2>&1
-echo "" > /dev/tty 2>&1
-
-# Restore output to the terminal for the final message
-exec > /dev/tty 2>&1
+# # Restore output to the terminal for the final message
+# exec > /dev/tty 2>&1
