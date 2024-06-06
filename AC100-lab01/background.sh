b8c64efc1efcb22b3b532538e4675c692c33e63e
David Chung <dchung@dchung.com>
2024-06-05 21:25:09 -0500

    moved tasls

--- AC100-lab01/background.sh
+++ AC100-lab01/background.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "Update package lists"
 # Install OpenJDK 11 without prompting for confirmation
-sudo apt-get install -y openjdk-11-jdk-headless < /dev/null > /dev/null 2>&1 
+sudo apt-get install -y openjdk-11-jdk-headless < /dev/null > /dev/null 
